9cd4cead825433d725d97238d60ff3751c8e55b6
Nurlitha Zhavierah <lithaazhavi@gmail.com>
2025-08-26 11:44:08 +0900

    day 1 belajar mobile

---
+++ tugas/bin/belajarday1.dart
@@ -0,0 +1,35 @@
+void main (){
+  // var name ="vira";
+  // print("haii $name");
+
+  // int number = 50;
+  // print(number);
+
+//type data
+  // string name = "vira";
+  // int number = 20;
+  // double pecahan = 20;
+  // num angka = 20.5;
+  // dynamic angka2 = "taehyungg";
+
+  // print(name);
+  // print(number);
+  // print(pecahan);
+  // print(angka);
+  // print(angka2);
+var name = "Nurlitha";
+String kuliah = "informatika/023"; //S nya huruf kapital
+int umur = 20;
+double pecahan = 60.5;
+num angka = 23.5;
+dynamic vira = "2 bersaudara" ;
+bool isActive = false;
+
+print ("heyy i am $name");
+print("kuliah di jurusan $kuliah");
+print("umur saya sekarang $umur tahun");
+print("bb saya $pecahan kg");
+print("angka fav saya $angka");
+print("saya $vira");
+print("mahasiswa ini statusnya $isActive");
+}
